646a5d79a9dde851026b507a2dc79884c44a55a4
SohelRihan <sohelrihan09@gmail.com>
2024-09-24 19:12:21 +0600

    calculate total donation

--- js/donation.js
+++ js/donation.js
@@ -1,8 +1,9 @@
-document.getElementById('btn-noakhali').addEventListener('click', function(){
-    
+// Noakhali Part
+document.getElementById('btn-noakhali').addEventListener('click', function () {
+
     const addDonationNoakhali = getMoneyById('input-noakhali');
 
-    if(addDonationNoakhali > 0){
+    if (addDonationNoakhali > 0) {
         const balanceNoakhali = getBalanceById('noakhali-donation-add');
         const noakhaliTotalDonation = balanceNoakhali + addDonationNoakhali;
 
@@ -13,9 +14,57 @@ document.getElementById('btn-noakhali').addEventListener('click', function(){
 
         document.getElementById('main-balance').innerText = aboveMainBalance;
 
-        
+
     }
-    else{
+    else {
+        alert('Failed to Donation!');
+    }
+});
+
+// Feni Part
+
+document.getElementById('btn-feni').addEventListener('click', function () {
+
+    const addDonationFeni = getMoneyById('input-feni');
+
+    if (addDonationFeni > 0) {
+        const balanceFeni = getBalanceById('feni-donation-add');
+        const feniTotalDonation = balanceFeni + addDonationFeni;
+
+        document.getElementById('feni-donation-add').innerText = feniTotalDonation;
+
+        const mainBalance = getTotalBalanceById('main-balance');
+        const aboveMainBalance = mainBalance - addDonationFeni;
+
+        document.getElementById('main-balance').innerText = aboveMainBalance;
+
+
+    }
+    else {
+        alert('Failed to Donation!');
+    }
+});
+
+// Quota Part
+
+document.getElementById('btn-quota').addEventListener('click', function () {
+
+    const addDonationQuota = getMoneyById('input-quota');
+
+    if (addDonationQuota > 0) {
+        const balanceQuota = getBalanceById('quota-donation-add');
+        const quotaTotalDonation = balanceQuota + addDonationQuota;
+
+        document.getElementById('quota-donation-add').innerText = quotaTotalDonation;
+
+        const mainBalance = getTotalBalanceById('main-balance');
+        const aboveMainBalance = mainBalance - addDonationQuota;
+
+        document.getElementById('main-balance').innerText = aboveMainBalance;
+
+
+    }
+    else {
         alert('Failed to Donation!');
     }
 });
--- js/feature.js
+++ js/feature.js
@@ -1,9 +1,11 @@
+// main balance function
 function getTotalBalanceById(id){
     const textTotalValue = document.getElementById(id).innerText;
     const textTotalNumber = parseFloat(textTotalValue);
     return textTotalNumber; 
 };
 
+// Donation part
 function getMoneyById(id){
     const inputValue = document.getElementById(id).value;
     const inputNumber = parseFloat(inputValue);
